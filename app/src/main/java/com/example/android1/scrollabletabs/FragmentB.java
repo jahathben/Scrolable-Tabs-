9f249cd2cfa8ec0146c93b485cf4ba639f0d93b1
Jahath Bennett <jahathben@gmail.com>
2015-04-27 01:51:24 -0400

    Scrollable tab folder

---
+++ app/src/main/java/com/example/android1/scrollabletabs/FragmentB.java
@@ -0,0 +1,19 @@
+package com.example.android1.scrollabletabs;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Android1 on 1/28/2015.
+ */
+public class FragmentB extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_b, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/android1/scrollabletabs/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.android1.scrollabletabs;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends FragmentActivity {
+
+    ViewPager viewPager = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        FragmentManager fragmentManager =getSupportFragmentManager();
+        viewPager.setAdapter(new MyAdapter(fragmentManager));
+    }
+
+    class MyAdapter extends FragmentPagerAdapter{
+
+        public MyAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            Fragment fragment = null;
+            if(i == 0){
+                fragment = new FragmentA();
+            }
+            if(i == 1){
+                fragment = new FragmentB();
+            }
+            if(i == 2){
+                fragment = new FragmentC();
+            }
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            if(position == 0){
+                return "Tab 1";
+            }
+            if(position == 1){
+                return "Tab 2";
+            }
+            if(position == 2){
+                return "Tab 3";
+            }
+            return null;
+        }
+    }
+}
